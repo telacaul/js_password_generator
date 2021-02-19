235c668e7f95694338dc6754f0073f743ce6f8fa
Tela Caul <telacaul@gmail.com>
2021-02-19 15:42:58 -0800

    reconfigured js file to make functions more clear and consolidated

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,172 +1,137 @@
-//Assignment Code
-
-//Variables
-//value of length of password
-var lengthPss= null; 
-
-//choice of character types (Boolean)
-userChoice = [
-  {
-    name: "lowercase characters",
-    value: false
-  },
-
-  {
-    name: "uppercase characters",
-    value: false
-  },
-  {
-    name: "numbers",
-    value: false
-  },
-  {
-    name: "special characters",
-    value: false
-  }
-];
-
-//character options for above character types
-var listCharacter = {
-  lettersLow: "abcdefghijklmnopqrstuvwxyz",
-  lettersUp: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
-  numbers: "0123456789",
-  specialChar: "!#$%&'()*+,-./:;<=>?@[]^_`{|}~",
-
-  //functions to pull options from above list
-
-  getLowerCase: function(){
-    return this.lettersLow;
-  },
-
-  getUpperCase: function(){
-    return this.lettersUp;
-  },
-
-  getNumbers: function(){
-    return this.numbers;
-  },
-
-  getSpecialChar: function(){
-    return this.specialChar;
-  }
-};
-
-//Functions
-//user interface
-
-var userIntro = function(){
-  //prompt for length of password
-  var info = prompt("Please type the length of the password\n" + 
-  "It must be between 8 and 128 characters");
-
-  //set range with boolean 
-  var range = false;
-
-  //validation or error message
-  while(!range){
-    //if input not a number
-    if(!isNaN(info)){
-      info = parseInt(info);
-      if (info >= 9 && info <= 128){
-        range = true;
-      }else{
-        alert("Error: You must choose a number between 8 and 128.");
-        info = prompt ("Please type the length of the password\n" + 
-        "It must be between 8 and 128 characters");
-      }
-    } else {
-      alert("You must choose a valid number");
-      info = prompt ("Ple`ase type the length of the password\n" + 
-      "It must be between 8 and 128 characters");
-    }
-  }
-
-  return info;
-};
-
-var userSelection = function(){
-  var x = 0; //user must select at least one option
-    while (x==0)
-    {
-      for(var i=0; i<userChoice.length; i++) {
-        userChoice[i].value = confirm("Would you like to include " + userChoice[i].name + "?");
-        if (userChoice[i].value){
-          x++;
-        }
-      }
+//unchanging (constant) variables for generator
+const criteria = {
+    numbers: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'],
+    lowerLetters: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
+    upperLetters: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'],
+    specials: ['!', '"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~'],
+    userChoices: [],
+    passwordLength: null,
+    lowerChoice: null,
+    upperChoice: null,
+    numbersChoice: null,
+    specialsChoice: null,
   
-    //make sure user chose one option
-  if(x==0){
-    alert("You have to choose at least one option for your password" +
-      "\n 1. " + userChoice[0].name+
-      "\n 2. " + userChoice[1].name+
-      "\n 3. " + userChoice[2].name+
-      "\n 4. " + userChoice[3].name);
+  };
+    
+  //*** GENERATING PASSWORD CRITERIA ***/
+  
+  //welcome alert and initiate user prompt for lowercase criteria
+  const beginPromptCriteria = () => {
+    alert("Welcome to Password Generator!\n" + "In your next steps, you must choose:\n" +
+    "** Length of password (between 8 and 128 characters)\n" +
+    "** Lowercase characters\n" +
+    "** Uppercase characters\n" +
+    "** Numbers\n" +
+    "** Special characters");
+    promptLowerCriteria();
+  };
+  
+  //uses lowerLetters array if 'yes' or moves to next prompt if 'cancel'
+  const promptLowerCriteria = () => {
+    let promptLower = confirm("Would you like to include lower-case letters?\nPress OK if yes or press Cancel if no");
+    criteria.lowerChoice = promptLower;
+    if (promptLower) {
+      criteria.userChoices.push(criteria.lowerLetters);
+    } 
+    promptUpperCriteria();
+  };
+  
+  //uses upperLetters array if 'yes' or moves to next prompt if 'cancel'
+  const promptUpperCriteria = () => {
+    let promptUpper = confirm('Would you like to include upper-case letters?\nPress OK if yes or press Cancel if no');
+    criteria.upperChoice = promptUpper;
+    if (promptUpper) {
+      criteria.userChoices.push(criteria.upperLetters);
     }
-  }
-  return x;
-};
-
-//Generate password function
-
-var generatePassword = function(){
-  var strong = "";
-  var pass = "";
-
-  //intro message
-
-  alert("Welcome to Password Generator!\n" + "In your next steps, you must choose:\n" +
-  "** Length of password (between 8 and 128 characters)\n" +
-  "** Lowercase characters\n" +
-  "** Uppercase characters\n" +
-  "** Numbers\n" +
-  "** Special characters");
-
-  lengthPss = userIntro();
-  var x = userSelection();
-
-  for(var i=0; i<Math.floor(lengthPss/x)+1; i++) {
-    if(userChoice[0].value){
-      strong += listCharacter.getLowerCase()[Math.floor(Math.random()*listCharacter.getLowerCase().length)];
+    promptNumbersCriteria();
+  };
+  
+  //uses numbers array if 'yes' or moves to next prompt if 'cancel'
+  const promptNumbersCriteria = () => {
+    let promptNumbers = confirm('Would you like to include numbers?\nPress OK if yes or press Cancel if no');
+    criteria.numbersChoice = promptNumbers;
+    if (promptNumbers) {
+      criteria.userChoices.push(criteria.numbers);
+    } 
+    promptSpecialsCriteria();
+  };
+  
+  //uses specials array if 'yes' or moves to validates criteria if 'cancel'
+  const promptSpecialsCriteria = () => {
+    let promptSpecials = confirm('Would you like to include special characters?\nPress OK if yes or press Cancel if no');
+    criteria.specialsChoice = promptSpecials;
+    if (promptSpecials) {
+      criteria.userChoices.push(criteria.specials);
+    } 
+    validateCriteria();
+  };
+  
+  //if clicked cancel on all criteria, cycles back through beginPromptCriteria
+  const validateCriteria = () => {
+    if ((criteria.lowerChoice === false) && (criteria.upperChoice === false) && (criteria.numbersChoice === false) && (criteria.specialsChoice === false)) {
+      alert('You must pick at least one criteria!');
+      beginPromptCriteria();
+    } else {
+      alert('Great! You have picked at least one criteria.\n\n Lets select your password length!');
     }
-
-    if(userChoice[1].value){
-      strong += listCharacter.getUpperCase()[Math.floor(Math.random()*listCharacter.getUpperCase().length)];
+    promptPasswordLength();
+  };
+  
+  //prompt for password length
+  const promptPasswordLength = () => {
+    let promptLength = prompt("Please type the length of the password\n" + 
+    "It must be between 8 and 128 characters");
+    //makes declaration an integer 
+    criteria.passwordLength = parseInt(promptLength, 10);
+  
+    if ((promptLength >= 8) && (promptLength <= 128)) {
+      alert('Great! We have all your criteria. Press the Generate Password button below to recieve your password. Click until you find one that suits you best!');
+    } else {
+      alert('Your password must be between 8 and 128 characters! Please try again.')
+      //brings user back to promptLength function
+      promptPasswordLength();
     }
-
-    if(userChoice[2].value){
-      strong += listCharacter.getNumbers()[Math.floor(Math.random()*listChar.getNumbers().length)];
+  };
+  
+  //randomly selects from user choices function
+  const randomUserChoices = () => {
+    let mergedChoices = criteria.userChoices.flat(1);
+    //merges into one function
+    return mergedChoices[Math.floor(Math.random() * mergedChoices.length)]
+  };
+  
+  
+  //generates password with specifications above
+  const generatePassword = () => {
+    let newPassword = [];
+  
+    for (i = 0; i < criteria.passwordLength; i++) {
+      newPassword.push(randomUserChoices());
     }
+  
+    return newPassword.join('');
+  };
 
-    if(userChoice[3].value){
-      strong += listCharacter.getSpecialChar()[Math.floor(Math.random()*listChar.getSpecialChar().length)];
+  //PUSH BUTTON TO GENERATE PASSWORD
+  const reset = {
+    resetBtn: document.createElement("button"),
+  
+  };
+  
+  // targets button to generate password
+  const generateBtn = document.querySelector("#generate");
+  
+  // password will be written if all criteria have been selected
+  const usePassBtn = () => {
+    if (!criteria.userChoices[0]) {
+      beginPromptCriteria();
+    } else {
+      let password = generatePassword();
+      let passwordText = document.querySelector("#password");
+      passwordText.value = password;
     }
-  }
-
-  for(var i=0; i<lengthPss; i++){
-    pass+= strong[Math.floor(Math.random()*str.length)];
-  }
-
-  return pass;
-}
-
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-  alert("Your password is:\n" + password);
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-
-
-
-
-// Need to figure out why password isn't generating - all prompts work correctly
+    
+  };
+  
+  // click button to generate password
+  generateBtn.addEventListener("click", usePassBtn);
