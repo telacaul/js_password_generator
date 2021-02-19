862131522aa94f12d40c93f1069a165f61aecb7f
Tela Caul <telacaul@gmail.com>
2021-02-18 19:33:35 -0800

    set up variables

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,57 @@
-// Assignment code here
+//Assignment Code
+
+//Variables
+//value of length of password
+var lengthPass= null; 
+
+//choice of character types (Boolean)
+userChoice = [
+  {
+    name: "lowercase characters",
+    value: false
+  },
+
+  {
+    name: "uppercase characters",
+    value: false
+  },
+  {
+    name: "numbers"
+    value: false
+  },
+  {
+    name: "special characters",
+    value: false
+  }
+];
+
+//character options for above character types
+var listCharacter = {
+  lettersLow: "abcdefghijklmnopqrstuvwxyz",
+  lettersUp: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+  numbers: "0123456789",
+  specialChar: "!#$%&'()*+,-./:;<=>?@[]^_`{|}~",
+
+  //functions to pull options from above list
+
+  getLowerCase: function(){
+    return this.lettersLow;
+  },
+
+  getUpperCase: function(){
+    return this.lettersUp;
+  },
+
+  getNumbers: function(){
+    return this.numbers;
+  },
+
+  getSpecialChar: function(){
+    return this.specialChar;
+  }
+};
+
+
 
 
 // Get references to the #generate element
@@ -10,8 +63,20 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+//genearate random password
+function generate(){
+
+//possible password values
+let values = "";
+
+let password = "";
+
+//for loop to choose password characters
+
+}
+
