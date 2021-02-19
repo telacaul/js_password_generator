688d0763fddf1fe7dd7ac49b6a19da857f8aec40
Tela Caul <telacaul@gmail.com>
2021-02-18 20:22:37 -0800

    updated alerts and first for-loop

--- Develop/script.js
+++ Develop/script.js
@@ -16,7 +16,7 @@ userChoice = [
     value: false
   },
   {
-    name: "numbers"
+    name: "numbers",
     value: false
   },
   {
@@ -51,6 +51,100 @@ var listCharacter = {
   }
 };
 
+//Functions
+//user interface
+
+var userIntro = function(){
+  //prompt for length of password
+  var info = prompt("Please type the length of the password\n" + 
+  "It must be between 8 and 128 characters");
+
+  //set range with boolean 
+  var range = false;
+
+  //validation or error message
+  while(!range){
+    //if input not a number
+    if(!isNaN(info)){
+      info = parseInt(info);
+      if (info >= 9 && info <= 128){
+        range = true;
+      }else{
+        alert("Error: You must choose a number between 8 and 128");
+        info = prompt ("Please type the length of the password\n" + 
+        "It must be between 8 and 128 characters");
+      }
+    } else {
+      alert("You must choose a valid number");
+      info = prompt ("Please type the length of the password\n" + 
+      "It must be between 8 and 128 characters");
+    }
+  }
+
+  return info;
+};
+
+var userSelection = function(){
+  var x = 0; //user must select at least one option
+    while (x===0)
+    {
+      for(var i=0; i<userChoice.length; i++) {
+        userChoice[i].value = confirm("Would you like to include " + userChoice[i].name);
+        if (userChoice[i].value){
+          x++;
+        }
+      }
+  
+    //make sure user chose one option
+  if(x===0){
+    alert("You have to choose at least one option for your password" +
+      "/n 1. " + userChoice[0].name+
+      "/n 2. " + userChoice[1].name+
+      "/n 3. " + userChoice[2].name+
+      "/n 4. " + userChoice[3].name+);
+    }
+  }
+  return x;
+};
+
+var generatePassword = function(){
+  var strong = "";
+  var pass = "";
+
+  alert("Welcome to Password Generator!/n"+"In your next steps, you must choose:\n" +
+  "** Length of password (between 8 and 128 characters)\n" +
+  "** Lowercase characters\n" +
+  "** Uppercase characters\n" +
+  "** Numbers\n" +
+  "** Special characters");
+
+  lengthPass = userIntro();
+  var x = userSelection();
+
+  for(var i=0, i<Math.floor(lengthPass/x)+1; i++) {
+    if(userChoice[0].value){
+      strong += listCharacter.getLowerCase()[Math.floor(Math.random()*listCharacter.getLowerCase().length)];
+    }
+
+    if(userChoice[1].value){
+      strong += listCharacter.getUpperCase()[Math.floor(Math.random()*listCharacter.getUpperCase().length)];
+    }
+
+    
+  }
+}
+
+
+
+ 
+
+
+
+
+
+
+
+
 
 
 
@@ -77,6 +171,7 @@ let values = "";
 let password = "";
 
 //for loop to choose password characters
-
+for (var i=0; i<Math.floor(lengthPass/x)+1; i++){
+}
 }
 
